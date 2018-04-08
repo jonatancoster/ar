7ea1d5d2c54e4c3a87bec1474e450c3ea766b79d
jonatancoster <jonatan.coster@gmail.com>
2018-04-08 15:49:17 +0200

    Shadows and translation working

---
+++ Assets/Data.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Data : MonoBehaviour {
+
+	public GameObject cube;
+	public GameObject ground;
+	public Text textfield;
+	public InputField namefield;
+
+	string filePath;
+	string filename;
+	string data;
+
+	// Use this for initialization
+	void Start () {
+		filePath = Application.persistentDataPath;
+		filename = "ar.txt";
+		data = "test";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void save() {
+		//System.IO.File.WriteAllText (filePath + "/" + filename, data);
+		System.IO.File.AppendAllText (filePath + "/" + filename, data);
+	}
+
+	public void saveName() {
+		System.IO.File.AppendAllText (filePath + "/" + filename, namefield.text, System.Text.Encoding.UTF8);
+	}
+
+	public void savePosition() {
+		System.IO.File.AppendAllText (filePath + "/" + filename, cube.transform.localPosition + "\n", System.Text.Encoding.UTF8);
+	}
+}
