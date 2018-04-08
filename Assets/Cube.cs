ea4acc80c17dc4eb2b183833521d0a30816c2c58
jonatancoster <jonatan.coster@gmail.com>
2018-04-08 13:13:50 +0200

    Added UI

---
+++ Assets/Cube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+	Vector3 distance = new Vector3(0f, 0.01f, 0f);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void MoveDown() {
+		this.gameObject.transform.Translate (-distance, Space.Self);
+	}
+
+	public void MoveUp() {
+		this.gameObject.transform.Translate (distance, Space.Self);
+	}
+}
