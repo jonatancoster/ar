e75a89e5c544c855a12466b05a5a838bc7374755
jonatancoster <jonatan.coster@gmail.com>
2018-04-10 11:16:23 +0200

    Added username to data file

--- Assets/Data.cs
+++ Assets/Data.cs
@@ -17,7 +17,7 @@ public class Data : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		filePath = Application.persistentDataPath;
-		filename = "ar.txt";
+		//filename = "ar.txt";
 		data = "test";
 	}
 	
@@ -26,17 +26,29 @@ public class Data : MonoBehaviour {
 		
 	}
 
+
+
 	public void save() {
 		//System.IO.File.WriteAllText (filePath + "/" + filename, data);
 		System.IO.File.AppendAllText (filePath + "/" + filename, data);
 	}
 
+
+
 	public void saveName() {
-		System.IO.File.AppendAllText (filePath + "/" + filename, namefield.text, System.Text.Encoding.UTF8);
+		//Create a file named after the user and add the user's name to the first line.
+		filename = namefield.text + ".txt";
+		System.IO.File.AppendAllText (filePath + "/" + filename, namefield.text + "\n", System.Text.Encoding.UTF8);
 	}
 
+
+
 	public void savePosition() {
+		
+		//Distance from ground to cube. Cube scale = 0.5.
 		float distance = (cube.transform.localPosition.y - 0.25f);
+
+		//Write the distance from ground to cube, cube local y position and cube global y position.
 		System.IO.File.AppendAllText (filePath + "/" + filename, distance.ToString() + "\t" + cube.transform.localPosition.y.ToString() + "\t" + cube.transform.position.y.ToString() + "\n", System.Text.Encoding.UTF8);
 	}
 }
